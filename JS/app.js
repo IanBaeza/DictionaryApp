d78f451aadc35243fc5bde56f174ef99233efa38
Nicolas Baeza <ianbaezarivera@gmail.com>
2024-03-08 21:06:10 -0300

    Se agrega API dictionary en pruebas

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+
+// API de palabras en ingles
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+const resultado = document.getElementById("resultado-box");
+const sonido = document.getElementById("sonido");
+const btnBuscar = document.getElementById("buscar-btn");
+
+btnBuscar.addEventListener("click", () => {
+        let inputPalabra = document.getElementById("palabra-input");
+        console.log(inputPalabra);
+    }
+);
